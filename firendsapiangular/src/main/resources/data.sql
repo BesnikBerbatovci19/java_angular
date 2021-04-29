353edfb7a999903b14532b419e4435e0ccd4a610
Besnik <51424232+BesnikBerbatovci19@users.noreply.github.com>
2021-04-29 16:11:23 +0200

    first commit

---
+++ firendsapiangular/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into Friend values(1,'Nigeria', 'Comp Sc', 'kindson@gmail.com', 'Kindson', 'Mononye');
+insert into Friend values (2, 'Hungary', 'Programing', 'mila@gmail.com', 'Nepla', 'Nepala');
+insert into Friend values (3, 'USA', 'Electronics', 'helen@gmail.com','Helen','philis');
+insert into Friend values (4, 'India', 'Informatics', 'Sharam@gmail.com','Sharama','Ravi');
+insert into Friend values (5, 'Canada', 'Security', 'Omar@gmail.com','Omar','Alabi');
+insert into Friend values (6, 'Kosovo', 'AddDev', 'Besnik@gmail.com','Besnik','Berbatovci');
---
+++ firendsapiangular/src/test/java/com/example/firendsapiangular/FirendsapiangularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firendsapiangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirendsapiangularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
